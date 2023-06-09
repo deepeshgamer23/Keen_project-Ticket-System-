beb5398d2ff1b18fe0edc031a058ea64b48d3a6c
deepesh <legendaryking624@gmail.com>
2023-06-09 10:29:11 +0530

    9/6/2023

--- src/app/components/SignUp/sign-up.component.ts
+++ src/app/components/SignUp/sign-up.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class SignUpComponent {
 
+  SignForm: any;
+  submitted: boolean = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.SignForm = this.formBuilder.group({
+      fullname:['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
+      number:['',Validators.required],
+      pass:['',Validators.required],
+      conPass:['',Validators.required],
+      checkbox:[false,Validators.requiredTrue],
+    })
+  }
+
+
+  get f(){return this.SignForm.controls}
+
+  onSignup(){
+    if(this.SignForm.invalid){
+      this.submitted = true;
+      return;
+    }
+    else{
+      this.submitted = false;
+      this.SignForm.reset();
+    }
+  }
 }
